91fd7b7c43b646f09ba56493881aaac5d7991907
Sergio Pinzon <sergiopin596@gmail.com>
2026-03-27 15:26:28 -0500

    feat: Add `checkRole` middleware to verify user permissions based on configured roles.

--- middleware/checkRole.js
+++ middleware/checkRole.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 
 const checkRole = () => {
     return async (interaction) => {
-        const hasRole = interaction.member.roles.cache.some(role => role.name === process.env.REQUIRED_ROLE);
+        // Separa los roles del .env por comas y quita espacios extra
+        const allowedRoles = process.env.REQUIRED_ROLE.split(',').map(role => role.trim());
+        
+        // Verifica si el usuario tiene al menos uno de los roles permitidos
+        const hasRole = interaction.member.roles.cache.some(role => allowedRoles.includes(role.name));
+        
         if (!hasRole) {
             await interaction.reply({
                 content: `No tienes permiso para usar este comando`,
